bb4b35530beffd2ce6ee5770ded11de5ef41dcfe
Gerardo <ge6975507@gmailcom>
2025-03-24 20:34:42 -0600

    Agregar archivos de proyecto.

---
+++ GABS2025324AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GABS2025324AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
